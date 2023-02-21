0dca6127ccce34574cc74c3ec920ffa76c985fbb
dosang13 <knight2671@gmail.com>
2023-02-22 02:51:37 +0900

    scroll add v1.02

--- js/project1_index.js
+++ js/project1_index.js
@@ -40,3 +40,30 @@ rightButton.addEventListener("click", () => {
   });
 });
 //box slide end
+
+//스크롤 이벤트 start
+window.addEventListener(
+  "wheel",
+  function (e) {
+    e.preventDefault();
+  },
+  { passive: false }
+);
+var mHtml = $("html");
+var page = 1;
+
+mHtml.animate({ scrollTop: 0 }, 10);
+
+$(window).on("wheel", function (e) {
+  if (mHtml.is(":animated")) return;
+  if (e.originalEvent.deltaY > 0) {
+    if (page == 7) return;
+    page++;
+  } else if (e.originalEvent.deltaY < 0) {
+    if (page == 1) return;
+    page--;
+  }
+  var posTop = (page - 1) * $(window).height();
+  mHtml.animate({ scrollTop: posTop });
+});
+//스크롤 이벤트 end
